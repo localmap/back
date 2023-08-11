4f4e39d24fbec5aeb698a42835737ced9983f325
byounghoonpark <88michael@naver.com>
2023-08-11 23:18:48 +0900

    FEAT event photo upload

--- localmap/events/serializers.py
+++ localmap/events/serializers.py
@@ -7,3 +7,9 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Events
         fields = '__all__'
+
+class EventSerializer_create(serializers.ModelSerializer):
+
+    class Meta:
+        model = Events
+        fields = ['rest_id','name','amount','content','url']
