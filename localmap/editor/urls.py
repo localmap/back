431b5ac0e7f5483101878da02e36bd8cb6371c0b
byounghoonpark <88michael@naver.com>
2023-08-18 16:35:12 +0900

    FIX ediotr url error

--- localmap/editor/urls.py
+++ localmap/editor/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('create/', views.editor_create, name='editor_create'),
     path('list/', views.editor_list, name='editor_list'),
     path('<uuid:pk>', views.editor_detail, name='editor_detail'),
-    path('update/<uuid:pk>', views.editor_update, name='editor_update'),
+#    path('update/<uuid:pk>', views.editor_update, name='editor_update'),
     path('delete/<uuid:pk>', views.editor_delete, name='editor_delete'),
     path('details/<uuid:pk>', views.editor_details, name='editor_details'),
 ]
